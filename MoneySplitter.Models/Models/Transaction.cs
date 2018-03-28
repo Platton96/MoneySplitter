564b439b22da81b2ff88e1990aa16dfa38ed3122
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-03-28 16:20:35 +0300

    change class Transaction

--- MoneySplitter.Models/Models/Transaction.cs
+++ MoneySplitter.Models/Models/Transaction.cs
@@ -1,11 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MoneySplitter.Models.Models
 {
     public class Transaction
     {
         public int Id { get; set; }
+        public User Debtor { get; set; }
+        public User Borrower { get; set; }
+        public double AmountOfMoney { get; set; }
+        public string NameTransaction { get; set; }
+        public DateTime DateTransaction { get; set; }
+        public bool IsActiveTransaction { get; set; }
     }
 }
--- MoneySplitter.Models/Models/User.cs
+++ MoneySplitter.Models/Models/User.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MoneySplitter.Models.Models
+﻿namespace MoneySplitter.Models.Models
 {
     public class User
     {
