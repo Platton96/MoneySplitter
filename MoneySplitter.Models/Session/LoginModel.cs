207196e88c050c953ba7f625830493654f0a30d7
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-03-29 15:15:42 +0300

    implements SessionApiService

---
+++ MoneySplitter.Models/Session/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System.Runtime.Serialization;
+
+namespace MoneySplitter.Models.Session
+{
+    [DataContract]
+    public class LoginModel
+    {
+        [JsonProperty]
+        [DataMember]
+        public string Email { get; set; }
+        [JsonProperty]
+        [DataMember]
+        public string Password { get; set; }
+    }
+}
--- MoneySplitter.Services/API/SessionApiServices.cs
+++ MoneySplitter.Services/API/SessionApiServices.cs
@@ -1,10 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using MoneySplitter.Models;
+using MoneySplitter.Models.Session;
+using System.Net;
 
 namespace MoneySplitter.Services.API
 {
     public class SessionApiServices
     {
+        private readonly string _urlSession;
+        private ApiBuilderURL _builderURL;
+        public SessionApiServices()
+        {
+            _builderURL = new ApiBuilderURL();
+            _urlSession = _builderURL.AddController("Session");
+        }
+        public async Task<UserModel> SignIn(LoginModel loginModel)
+        {
+            var signInUri = new Uri(_builderURL.AddMethodOfController(_urlSession, "SignIn"));
+            var result = new UserModel();
+
+            using (var httpClient = new HttpClient())
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(loginModel), Encoding.UTF8, "application/json");
+                var responce = await httpClient.PostAsync(signInUri, content);
+                if (responce.StatusCode == HttpStatusCode.OK)
+                {
+                    var jsonLogin = await responce.Content.ReadAsStringAsync();
+                    result = JsonConvert.DeserializeObject<UserModel>(jsonLogin);
+                }
+                else
+                {
+                    result=null;
+                }
+            }
+
+            return result;
+        }
     }
 }
