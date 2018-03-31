33b8e92e695d1af4f39eb25ca8da6099a82acc31
Кульбеда Сергей <sergey_kulbeda@mail.ru>
2018-03-31 20:25:19 +0300

    Add RegistrModel

--- MoneySplitter.Infrastructure/ISessionApiService.cs
+++ MoneySplitter.Infrastructure/ISessionApiService.cs
@@ -1,10 +1,11 @@
 ﻿using System.Threading.Tasks;
-using MoneySplitter.Models;
+using MoneySplitter.Models.Session;
 
 namespace MoneySplitter.Infrastructure
 {
     public interface ISessionApiService<T> where T: class
     {
         Task<T> SignInAsync(string email, string password);
+        Task<T> RegistrAsync(RegistrModel registrModel);
     }
 }
---
+++ MoneySplitter.Models/Session/RegistrModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Serialization;
+
+namespace MoneySplitter.Models.Session
+{
+    [DataContract]
+    public class RegistrModel
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Surname { get; set; }
+
+        [DataMember]
+        public string Email { get; set; }
+
+        [DataMember]
+        public int PhoneNumber { get; set; }
+
+        [DataMember]
+        public int CreditCardNumber { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+
+        
+
+    }
+}
