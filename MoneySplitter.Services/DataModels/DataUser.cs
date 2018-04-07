807f215dc0792cebc37cc46983cda257782e72de
Кульбеда Сергей <sergey_kulbeda@mail.ru>
2018-04-07 11:08:58 +0300

    remove useless lib

--- MoneySplitter.Services/DataModels/DataUser.cs
+++ MoneySplitter.Services/DataModels/DataUser.cs
@@ -1,5 +1,4 @@
-﻿using MoneySplitter.Infrastructure;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Runtime.Serialization;
 
 namespace MoneySplitter.Services.DataModels
