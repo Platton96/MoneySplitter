6244ddba4e22fb4ec8cc2e21b032c20c4dd1cf80
Кульбеда Сергей <sergey_kulbeda@mail.ru>
2018-04-08 23:35:59 +0300

    add dataGetUser

---
+++ MoneySplitter.Services/DataModels/DataGetUser.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace MoneySplitter.Services.DataModels
+{
+    [DataContract]
+    public class DataGetUser
+    {
+        [DataMember]
+        public string Email { get; set; }
+
+        [DataMember]
+        public string Token { get; set; }
+    }
+}
