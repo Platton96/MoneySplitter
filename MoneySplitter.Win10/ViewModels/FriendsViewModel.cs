73913e49e4a07181ae4b90583e51df5850921a52
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-04-09 15:45:29 +0300

    get Friends into View

--- MoneySplitter.Win10/ViewModels/FriendsViewModel.cs
+++ MoneySplitter.Win10/ViewModels/FriendsViewModel.cs
@@ -1,8 +1,37 @@
 ﻿using Caliburn.Micro;
+using MoneySplitter.Infrastructure;
+using MoneySplitter.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MoneySplitter.Win10.ViewModels
 {
     public class FriendsViewModel : Screen
     {
+        private IEnumerable<UserModel> _friends;
+
+        private IFriendsManager _friendsManager;
+
+        public IEnumerable<UserModel> Friends
+        {
+            get { return _friends; }
+            set
+            {
+                _friends = value;
+                NotifyOfPropertyChange(nameof(Friends));
+            }
+        }
+
+        public FriendsViewModel(IFriendsManager friendsManager)
+        {
+            _friendsManager = friendsManager;
+        }
+
+        protected override void OnActivate()
+        {
+            base.OnActivate();
+            Friends = _friendsManager.FriendsOfCurentUser;
+        }
+
     }
 }
--- MoneySplitter.Win10/Views/FriendsView.xaml.cs
+++ MoneySplitter.Win10/Views/FriendsView.xaml.cs
@@ -1,12 +1,16 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using MoneySplitter.Win10.ViewModels;
+using Windows.UI.Xaml.Controls;
 
 namespace MoneySplitter.Win10.Views
 {
     public sealed partial class FriendsView : Page
     {
+        public FriendsViewModel ViewModel { get; set; }
+
         public FriendsView()
         {
             InitializeComponent();
+            DataContextChanged += (s, e) => { ViewModel = DataContext as FriendsViewModel; };
         }
     }
 }
