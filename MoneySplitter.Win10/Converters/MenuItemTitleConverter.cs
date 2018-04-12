860703668130ae6b098bc272cb386aaf85dea967
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-04-12 18:09:42 +0300

    add converter

---
+++ MoneySplitter.Win10/Converters/MenuItemTitleConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+using Windows.UI.Xaml.Data;
+
+namespace MoneySplitter.Win10.Converters
+{
+    public class MenuItemTitleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var index = int.Parse(parameter.ToString());
+            var str = value.ToString();
+
+            var indexOfFirstSpace = str.IndexOf(' ');
+
+            return index == 0 ? HttpUtility.HtmlDecode(str.Substring(0, indexOfFirstSpace)) : str.Substring(indexOfFirstSpace + 1, str.Length - indexOfFirstSpace - 1);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
