56d8c0f3ba90d91c9513431e55b0b73ae9bbb0a0
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-04-12 12:11:01 +0300

    add defines

---
+++ MoneySplitter.Win10/Defines.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MoneySplitter.Win10
+{
+    public static class Defines
+    {
+        public static class Title
+        {
+            public const string HOME = "Home";
+            public const string FRIENDS = "Friends";
+            public const string SEARCH = "Search";
+        }
+
+        public static class IconButton
+        {
+            public const string HOME = "&#xE80F;";
+            public const string FRIENDS = "&#xE716;";
+            public const string SEARCH = "&#xE721;";
+        }
+    }
+}
