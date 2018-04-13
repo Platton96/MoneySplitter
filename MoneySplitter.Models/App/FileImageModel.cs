02359e85557779d15b8f4f72788993eb8fc2a706
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-04-13 13:30:29 +0300

    add FileImageModel

---
+++ MoneySplitter.Models/App/FileImageModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace MoneySplitter.Models.App
+{
+    public class FileImageModel
+    {
+        public string ImageName { get; set; }
+        public string Base64StringImage { get; set; }
+    }
+}
