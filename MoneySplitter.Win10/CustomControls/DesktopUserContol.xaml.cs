933336c47db9270d801c6a7d951cfbd022aecfdd
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-04-14 16:55:31 +0300

    add desktopControl

---
+++ MoneySplitter.Win10/CustomControls/DesktopUserContol.xaml.cs
@@ -0,0 +1,47 @@
+﻿using MoneySplitter.Models;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MoneySplitter.Win10.CustomControls
+{
+    public sealed partial class DesktopUserContol : UserControl
+    {
+
+        public event EventHandler<UserModel> OnAddOrRemoveItemClick;
+
+        public UserModel ViewModel
+        {
+            get { return (UserModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(
+            "ViewModel",
+            typeof(UserModel),
+            typeof(DesktopUserContol),
+            null);
+
+        public string ButtonContent
+        {
+            get { return (string)GetValue(ButtonContentProperty); }
+            set { SetValue(ButtonContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty ButtonContentProperty = DependencyProperty.Register(
+            "ButtonContent",
+            typeof(string),
+            typeof(DesktopUserContol),
+            null);
+
+        public DesktopUserContol()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAddOrRemoveButtonClick(object sender, RoutedEventArgs e)
+        {
+            OnAddOrRemoveItemClick?.Invoke(this, ViewModel);
+        }
+    }
+}
