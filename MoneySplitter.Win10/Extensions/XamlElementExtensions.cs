9da41de6a6043b85b4da538aaef5ce80e43ff3ac
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-04-14 16:58:29 +0300

    XanlElementExtension

---
+++ MoneySplitter.Win10/Extensions/XamlElementExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MoneySplitter.Win10.Extensions
+{
+    public static class XamlElementExtensions
+    {
+        public static readonly DependencyProperty ResourceProperty =
+        DependencyProperty.RegisterAttached("Resource", typeof(string), typeof(XamlElementExtensions), new PropertyMetadata(string.Empty, OnResourceChanged));
+        public static string GetResource(DependencyObject obj) { return (string)obj.GetValue(ResourceProperty); }
+        public static void SetResource(DependencyObject obj, string value) { obj.SetValue(ResourceProperty, value); }
+
+        private static void OnResourceChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            var control = (FrameworkElement)dependencyObject;
+
+            if (control is TextBlock textBlock)
+            {
+                // TODO get string from service
+                textBlock.Text = e.NewValue.ToString();
+            }
+
+        }
+    }
+}
