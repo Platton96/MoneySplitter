497a151e5ae28dcaff1f9a3e9d96d55ce3ba5ff0
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-04-18 17:58:02 +0300

    add executor

---
+++ MoneySplitter.Services/Executor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneySplitter.Services
+{
+    public class Executor
+    {
+        public bool IsSuccesExecute()
+        {
+            return true;
+        }
+    }
+}
