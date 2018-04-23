cb11c672e797634df88deedd9d13e51bbdd7aa28
Кульбеда Сергей <sergey_kulbeda@mail.ru>
2018-04-23 15:42:31 +0300

    add loading ring

---
+++ MoneySplitter.Win10/CustomControls/LoadRingControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MoneySplitter.Win10.CustomControls
+{
+    public sealed partial class LoadRingControl : UserControl
+    {
+        public LoadRingControl()
+        {
+            InitializeComponent();
+        }
+        public bool IsActiveRing
+        {
+            get { return (bool)GetValue(IsActiveRingPropery); }
+            set { SetValue(IsActiveRingPropery, value); }
+        }
+
+        public static readonly DependencyProperty IsActiveRingPropery = DependencyProperty.Register(
+            "IsActiveRing",
+            typeof(bool),
+            typeof(LoadRingControl),
+            null);
+    }
+}
