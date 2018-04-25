840bf850561d72c0242cca2fd3b4e27d1d98614f
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-04-25 16:52:33 +0300

    add issue model

---
+++ MoneySplitter.Models/App/IssueModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MoneySplitter.Models.App
+{
+    public class IssueModel
+    {
+        public bool IsIssueVisibility { get; set; }
+        public string IssueTitle { get; set; }
+        public string IssueMessage { get; set; }
+    }
+}
