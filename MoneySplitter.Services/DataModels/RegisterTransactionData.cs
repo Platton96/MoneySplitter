5dfec94b2dd10f40e7090fabd7aed44bb46ae354
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-04-28 12:22:25 +0300

    add RegisterTransactionData

---
+++ MoneySplitter.Services/DataModels/RegisterTransactionData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace MoneySplitter.Services.DataModels
+{
+    [DataContract]
+    public class RegisterTransactionData
+    {
+        [DataMember]
+        public string Email { get; set; }
+
+        [DataMember]
+        public string Token { get; set; }
+
+        [DataMember]
+        public double Cost { get; set; }
+
+        [DataMember]
+        public IEnumerable<int> CollaboratorsIds { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public DateTime DeadlineDate { get; set; }
+
+        [DataMember]
+        public string ImageBase64String { get; set; }
+    }
+}
