527b8b24bf7cdba7fc8539ff6569f14d3dc70db0
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-04-28 16:50:25 +0300

    implement Dependencies

---
+++ MoneySplitter.Services/Dependencies.cs
@@ -0,0 +1,10 @@
+﻿using Caliburn.Micro;
+using MoneySplitter.Infrastructure;
+
+namespace MoneySplitter.Services
+{
+    public class Dependencies
+    {
+        public IMembershipService MembershipService => IoC.Get<IMembershipService>();
+    }
+}
