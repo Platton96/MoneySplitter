89bbdba4add4bb42e0570296837904d99d18fbbe
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-04-28 12:22:50 +0300

    add TransactionData

---
+++ MoneySplitter.Services/DataModels/TransactionData.cs
@@ -0,0 +1,44 @@
+﻿using MoneySplitter.Models;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace MoneySplitter.Services.DataModels
+{
+    [DataContract]
+    public class TransactionData
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public DateTime DeadlineDate { get; set; }
+
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public DateTime CreationDate { get; set; }
+
+        [DataMember]
+        public UserModel Owner { get; set; }
+
+        [DataMember]
+        public IEnumerable<UserModel> Collaborators { get; set; }
+
+        [DataMember]
+        public IEnumerable<UserModel> Finished { get; set; }
+
+        [DataMember]
+        public IEnumerable<UserModel> InProgress { get; set; }
+
+        [DataMember]
+        public double Coast { get; set; }
+
+        [DataMember]
+        public string ImageUrl { get; set; }
+    }
+}
