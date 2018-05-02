ba6cc8a90aa2965bb8fe76de06a7bcd789803d21
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-05-02 15:26:01 +0300

    implemet TransactionsControl

---
+++ MoneySplitter.Win10/CustomControls/TransctionsControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿using MoneySplitter.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace MoneySplitter.Win10.CustomControls
+{
+    public sealed partial class TransctionsControl : UserControl
+    {
+        public TransactionModel ViewModel
+        {
+            get { return (TransactionModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+        public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(
+            "ViewModel",
+            typeof(TransactionModel),
+            typeof(TransctionsControl),
+            null);
+        public TransctionsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
