eff5a9d4bfdd4dfd330d9b56a1b790eda54b8321
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-05-03 11:47:36 +0300

    moved services back

---
+++ MoneySplitter.Services/Api/ApiUrlBuilder.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using MoneySplitter.Infrastructure;
+
+namespace MoneySplitter.Services.Api
+{
+    public  class ApiUrlBuilder : IApiUrlBuilder
+    {
+        public Uri Authorization()
+        {
+            return new Uri(string.Concat(
+                Defines.Api.WEB_API_URL,
+                Defines.Api.Session.SESSION,
+                Defines.Api.Session.SIGN_IN));
+        }
+
+        public Uri Register()
+        {
+            return new Uri(string.Concat(
+                Defines.Api.WEB_API_URL,
+                Defines.Api.Session.SESSION,
+                Defines.Api.Session.REGISTER));
+        }
+
+        public Uri SearchUsers(string query)
+        {
+            return new Uri(string.Concat(
+                Defines.Api.WEB_API_URL,
+                Defines.Api.Users.USERS,
+                Defines.Api.Users.SEARCH,
+                Defines.Api.Users.SEARCH_PARAMETR,
+                query));
+        }
+
+        public Uri AddFriend()
+        {
+            return new Uri(string.Concat(
+                Defines.Api.WEB_API_URL,
+                Defines.Api.Friends.FRIENDS,
+                Defines.Api.Friends.ADD_FRIEND));
+        }
+
+        public Uri RemoveFriend()
+        {
+            return new Uri(string.Concat(
+                Defines.Api.WEB_API_URL,
+                Defines.Api.Friends.FRIENDS,
+                Defines.Api.Friends.REMOVE_FRIEND));
+        }
+
+        public Uri AllFriends()
+        {
+            return new Uri(string.Concat(
+             Defines.Api.WEB_API_URL,
+             Defines.Api.Friends.FRIENDS,
+             Defines.Api.Friends.ALL_FRIENDS));
+        }
+
+        public Uri AllUserTransactions()
+        {
+            return new Uri(string.Concat(
+                Defines.Api.WEB_API_URL,
+                Defines.Api.Transactions.TRANSACTIONS,
+                Defines.Api.Transactions.MY));
+        }
+
+        public Uri AddTransaction()
+        {
+            return new Uri(string.Concat(
+                Defines.Api.WEB_API_URL,
+                Defines.Api.Transactions.TRANSACTIONS,
+                Defines.Api.Transactions.ADD));
+        }
+
+        public Uri AllTransactions()
+        {
+            return new Uri(string.Concat(
+                Defines.Api.WEB_API_URL,
+                Defines.Api.Transactions.TRANSACTIONS,
+                Defines.Api.Transactions.ALL));
+        }
+    }
+}
---
+++ MoneySplitter.Services/Api/SearchApiService.cs
@@ -0,0 +1,55 @@
+﻿using MoneySplitter.Infrastructure;
+using MoneySplitter.Models;
+using MoneySplitter.Services.DataModels;
+using MoneySplitter.Services.Inerfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneySplitter.Services.Api
+{
+    public class SearchApiService : ISearchApiService
+    {
+        private readonly IApiUrlBuilder _apiUrlBuilder;
+        private readonly IQueryApiService _queryApiService;
+        private readonly IMapper _mapper;
+        private readonly IExecutor _executor;
+
+        public SearchApiService(IApiUrlBuilder apiUrlBuilder, IQueryApiService queryApiService, IMapper mapper, IExecutor executor)
+        {
+            _apiUrlBuilder = apiUrlBuilder;
+            _queryApiService = queryApiService;
+            _mapper = mapper;
+            _executor = executor;
+        }
+
+        public async Task<ExecutionResult<IEnumerable<UserModel>>> SearchUsersAsync(string query)
+        {
+            var result = new ExecutionResult<IEnumerable<UserModel>>
+            {
+                IsSuccess = false
+            };
+
+            var searchUsersUrl = _apiUrlBuilder.SearchUsers(query);
+
+            IEnumerable<UserData> usersData = null;
+
+            await _executor.ExecuteWithRetryAsync(async () =>
+            {
+                usersData = await _queryApiService.GetAsync<IEnumerable<UserData>>(searchUsersUrl);
+            });
+
+            if (usersData == null)
+            {
+                return result;
+            }
+
+            return new ExecutionResult<IEnumerable<UserModel>>
+            {
+                IsSuccess = true,
+                Result = usersData.Select(x => _mapper.ConvertUserDataToUserModel(x)).ToList()
+            };
+
+        }
+    }
+}
