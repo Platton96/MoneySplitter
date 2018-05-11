4f022bb871dc5e268dae0975329d2a7ae4170592
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-05-11 19:50:24 +0300

    edit collabaratorModel

--- MoneySplitter.Models/CollabaratorModel.cs
+++ MoneySplitter.Models/CollabaratorModel.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace MoneySplitter.Models
 {
-    class Collabarator
+    public class CollabaratorModel
     {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public COLLABARATOR_STATUS CollabaratorStatus { get; set; }
+        public TRANSACTION_STAYUS TransactionStatus { get; set; }
+        public double Cost { get; set; }
+    }
+
+    public enum COLLABARATOR_STATUS
+    {
+        OneDebt,
+        ManyDebt,
+        OneLend,
+        ManyLend
+    }
+
+    public enum TRANSACTION_STAYUS
+    {
+        InBegin,
+        InProgress,
+        InFinish,
+        Undefined
     }
 }
