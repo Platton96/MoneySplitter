4b1c04f6644a30679e7374c366660dd3df6a48df
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-05-11 19:59:20 +0300

    add field for viewmodel

--- MoneySplitter.Win10/ViewModels/IncomingAndOutcomingViewModel.cs
+++ MoneySplitter.Win10/ViewModels/IncomingAndOutcomingViewModel.cs
@@ -1,13 +1,64 @@
 ﻿using Caliburn.Micro;
-using System;
-using System.Collections.Generic;
+using MoneySplitter.Infrastructure;
+using MoneySplitter.Models;
+using MoneySplitter.Models.App;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MoneySplitter.Win10.Common;
+
 
 namespace MoneySplitter.Win10.ViewModels
 {
     public class IncomingAndOutcomingViewModel :Screen
     {
+        #region Fields
+        private ObservableCollection<CollabaratorModel> _debtors;
+        private ObservableCollection<CollabaratorModel> _lendPersons;
+
+        private bool _isNotTransactionsTextVisibility;
+
+        private INavigationManager _navigationManager;
+        private CollabaratorModelFactory _collabaratorModelFactory;
+        #endregion
+
+        #region Properties
+        public ObservableCollection<CollabaratorModel> Debtors
+        {
+            get { return _debtors; }
+            set
+            {
+                _debtors = value;
+                NotifyOfPropertyChange(nameof(Debtors));
+            }
+        }
+
+        public ObservableCollection<CollabaratorModel>LendPersons
+        {
+            get { return _lendPersons; }
+            set
+            {
+                _lendPersons = value;
+                NotifyOfPropertyChange(nameof(LendPersons));
+            }
+        }
+
+        public bool IsNotTransactionsTextVisibility
+        {
+            get { return _isNotTransactionsTextVisibility; }
+            set
+            {
+                _isNotTransactionsTextVisibility = value;
+                NotifyOfPropertyChange(nameof(IsNotTransactionsTextVisibility));
+            }
+        }
+        #endregion
+        public IncomingAndOutcomingViewModel(CollabaratorModelFactory collabaratorModelFactory, INavigationManager navigationManager)
+        {
+            _collabaratorModelFactory = collabaratorModelFactory;
+            _navigationManager = navigationManager;
+        }
+      
+
+
     }
 }
--- MoneySplitter.Win10/Views/IncomingAndOutcomingView.xaml.cs
+++ MoneySplitter.Win10/Views/IncomingAndOutcomingView.xaml.cs
@@ -12,19 +12,17 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+using MoneySplitter.Win10.ViewModels;
 
 namespace MoneySplitter.Win10.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class IncomingAndOutcomingView : Page
     {
+        IncomingAndOutcomingViewModel ViewModel { get; set; }
         public IncomingAndOutcomingView()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            DataContextChanged += (s, e) => { ViewModel = DataContext as IncomingAndOutcomingViewModel; };
         }
     }
 }
