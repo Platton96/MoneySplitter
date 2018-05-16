722ab12de631734d6fc726bea01b7bf6f6136d04
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-05-16 11:57:29 +0300

    implement StatusesModel

---
+++ MoneySplitter.Win10/CustomControls/Models/StatusesModel.cs
@@ -0,0 +1,19 @@
+﻿using MoneySplitter.Models;
+using System;
+
+namespace MoneySplitter.Win10.CustomControls.Models
+{
+    public class StatusesModel
+    {
+        public TransactionStatus TransactionStatus { get; set; }
+        public CollabaratorStatus CollabaratorStatus { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as StatusesModel;
+            return TransactionStatus == other.TransactionStatus &&
+                 CollabaratorStatus == other.CollabaratorStatus;
+        }
+
+    }
+}   
