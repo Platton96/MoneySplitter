09877be10fd4d7090d59e8adcb82ac6aa9039dd6
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-05-28 19:29:49 +0300

    work with desain

---
+++ MoneySplitter.Win10/Converters/TypeDateCoverter.cs
@@ -0,0 +1,33 @@
+﻿using MoneySplitter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace MoneySplitter.Win10.Converters
+{
+    public class TypeDateCoverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            var typeDate = (TypeDate)value;
+
+            if(typeDate==TypeDate.DEADLINE_DATE)
+            {
+                return "Deadline date: ";
+            }
+            return "Event date: ";
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
