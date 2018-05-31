65417ce7941db45d077bdaa3e659acd4ad8f0dd1
Uladzimir Paliukhovich <uladzimir_paliukhovich@epam.com>
2018-05-31 10:58:50 +0300

    Task 14. Try to push

--- MoneySplitter.Win10/Common/CollaboratorModelFactory.cs
+++ MoneySplitter.Win10/Common/CollaboratorModelFactory.cs
@@ -109,6 +109,7 @@ namespace MoneySplitter.Win10.Common
             }
 
             var firstRecord = collabaratorRecords.FirstOrDefault();
+
             return new CollaboratorModel
             {
                 Email = firstRecord.Email,
