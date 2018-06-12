cc78a87393d0c4e8cf5e06e3ce9065e28b9ce3ee
Siarhei Kulbeda <Siarhei_Kulbeda@epam.com>
2018-06-12 12:41:43 +0300

    add Resurce

---
+++ MoneySplitter.Win10/Common/LocalisationServise.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneySplitter.Win10.Common
+{
+    public class LocalisationServise
+    {
+        private readonly ResourceManager _resourseManger;
+
+        public LocalisationServise()
+        {
+            _resourseManger=new ResourceManager("UsingRESX.Strings/Ru",
+                Assembly.GetExecutingAssembly());
+        }
+    }
+}
