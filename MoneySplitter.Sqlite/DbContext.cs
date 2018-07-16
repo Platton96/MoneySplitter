f15555a01b109e373d30910039cb8a4362db74f0
Кульбеда Сергей <sergey_kulbeda@mail.ru>
2018-07-16 21:59:06 +0300

    add new create table methods

--- MoneySplitter.Sqlite/DbContext.cs
+++ MoneySplitter.Sqlite/DbContext.cs
@@ -20,19 +20,6 @@ namespace MoneySplitter.Sqlite
 
         public async Task CreateUsersTableAsync()
         {
-            await DbConection.OpenAsync();
-
-            SqliteCommand deleteUsersCommand = new SqliteCommand("DROP TABLE Users;", DbConection);
-
-            try
-            {
-                await deleteUsersCommand.ExecuteNonQueryAsync();
-            }
-            catch 
-            {
-
-            }
-
             SqliteCommand createUsersCommand = new SqliteCommand(@"
 				CREATE TABLE Users (
 				Id int PRIMARY KEY,
@@ -45,9 +32,63 @@ namespace MoneySplitter.Sqlite
 				ImageUrl nvarchar(255),
 				BackgroundImageUrl nvarchar(255));",
                 DbConection);
+            await CreateTableAsync(Defines.SQlilte.TableNammes.USERS, createUsersCommand);
+        }
 
-            await createUsersCommand.ExecuteNonQueryAsync();
+        public async Task CreateFriendsTableAsync()
+        {
+            SqliteCommand createFriendsCommand = new SqliteCommand(@"
+				CREATE TABLE Friends (
+				Id int PRIMARY KEY,
+				Name nvarchar(255),
+				Surname nvarchar(255),
+				Email nvarchar(255),
+				PhoneNumber bigint,
+				CreditCardNumber bigint,
+				Token nvarchar(255),
+				ImageUrl nvarchar(255),
+				BackgroundImageUrl nvarchar(255));",
+                DbConection);
+
+            await CreateTableAsync(Defines.SQlilte.TableNammes.FRIENDS, createFriendsCommand);
+        }
+
+        public async Task CreateTransactionsTableAsync()
+        {
+            SqliteCommand createFriendsCommand = new SqliteCommand(@"
+				CREATE TABLE Friends (
+				Id int PRIMARY KEY,
+				Name nvarchar(255),
+				Surname nvarchar(255),
+				Email nvarchar(255),
+				PhoneNumber bigint,
+				CreditCardNumber bigint,
+				Token nvarchar(255),
+				ImageUrl nvarchar(255),
+				BackgroundImageUrl nvarchar(255));",
+                DbConection);
+
+            await CreateTableAsync(Defines.SQlilte.TableNammes.TRANSACTIONS, createFriendsCommand);
+        }
+
+        public async Task CreateTableAsync(string nameTable, SqliteCommand createTableCommand)
+        {
+            await DbConection.OpenAsync();
+
+            SqliteCommand deleteFriendsCommand = new SqliteCommand($"DROP TABLE {nameTable};", DbConection);
+
+            try
+            {
+                await deleteFriendsCommand.ExecuteNonQueryAsync();
+            }
+            catch
+            {
+
+            }
+
+            await createTableCommand.ExecuteNonQueryAsync();
             DbConection.Close();
         }
+
     }
 }
--- MoneySplitter.Sqlite/Defines.cs
+++ MoneySplitter.Sqlite/Defines.cs
@@ -5,6 +5,12 @@
         public static class SQlilte
         {
             public const string CONECTION_STRING = "Filename=MoneySplitter.db";
+            public static class TableNammes
+            {
+                public const string USERS = "Users";
+                public const string FRIENDS = "Friends";
+                public const string TRANSACTIONS = "Transactions";
+            }
         }
     }
 }
